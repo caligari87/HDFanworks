b1324cfc6d4ce17d2dc7607b698438eaa45c0019
Sterling Parker <caligari.87@gmail.com>
2020-11-13 22:21:39 -0700

    Update build script to generate HTML

--- buildthumbs.sh
+++ buildthumbs.sh
@@ -5,4 +5,6 @@ cd temp/
 for F in *.*
 do
 	convert "$F" -format jpg -resize 'x300>' ../thumbs/"${F%%.*}".jpg
+	mv $F ../images
+	echo "<a href=\"images/$F\"><img class=\"thumb\" src=\"thumbs/${F%%.*}.jpg\"></a>"
 done
